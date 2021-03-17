b88fb8b6a20955c978a5b8a94846c69cef6877b3
jesusMava <jesusmacedo7@gmail.com>
2021-03-17 12:14:42 -0600

    Creating routes and links

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,14 +5,27 @@ import {
   Tabs,
   Tab
 } from '@material-ui/core/';
+import { 
+  Link,
+  useLocation
+} from 'react-router-dom';
 
 const useStyles = makeStyles({
   root: {
     flexGrow: 1,
+    marginBottom: '30px',
+    backgroundColor: '#bec7d4'
   }
 })
 const NavBar = () => {
-  const [option, setOption] = React.useState(0);
+  let { pathname } = useLocation();
+  const myPageIndex = {
+    '/users': 0,
+    '/movies': 1,
+    '/history': 2,
+    '/': 0
+  }
+  const [option, setOption] = React.useState(myPageIndex[pathname]);
   const classes = useStyles();
   const handleChange = (event, newValue) => {
     setOption(newValue);
@@ -23,13 +36,13 @@ const NavBar = () => {
       <Tabs
         value={option}
         onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
+        indicatorColor='primary'
+        textColor='primary'
         centered
       >
-        <Tab label="Users" />
-        <Tab label="Movies" />
-        <Tab label="History" />
+        <Tab label='Users' component={Link} to='/users'/>
+        <Tab label='Movies' component={Link} to='/movies'/>
+        <Tab label='History' component={Link} to='/history'/>
       </Tabs>
     </Paper>
   ); 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from './routes/routes'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      <Routes />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
